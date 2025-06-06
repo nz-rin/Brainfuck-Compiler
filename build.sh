be49e67b8cd705d92c2598d2c01b5fe958e474b4
Rin <namelezzrin@gmail.com>
2025-06-06 13:09:53 +0200

    fixed build.sh, added shebang, and more compilation info

--- build.sh
+++ build.sh
@@ -1,2 +1,10 @@
-echo "compiling"
+#!/usr/bin/env bash
+
+echo "COMPILING"
 g++ -Iinclude -Wall -Wextra ./src/* -o ./build/bfc
+
+if [[ $? != 0 ]]; then
+	echo "FAILED"
+else
+	echo "COMPILED SUCCESSFULLY"
+fi
